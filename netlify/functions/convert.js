c58a8926050af43be87289f8f9bcc9773bf7cf81
Almasy74 <trond.ausland@gmail.com>
2025-06-08 14:15:56 +0200

    Add files via upload

--- netlify/functions/convert.js
+++ netlify/functions/convert.js
@@ -1,1 +1,15 @@
+const axios = require('axios');
 
+exports.handler = async (event) => {
+  const { from, to, amount } = event.queryStringParameters;
+  try {
+    const res = await axios.get(`https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`);
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(res.data)
+    };
+  } catch (e) {
+    return { statusCode: 500, body: 'Error retrieving exchange rate.' };
+  }
+};
