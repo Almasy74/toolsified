29db043bf18fbe1cea728c9dea22f7a82522b77e
Almasy74 <trond.ausland@gmail.com>
2025-09-29 16:56:09 +0200

    Update crawls_ds.py

--- scripts/python/crawls_ds.py
+++ scripts/python/crawls_ds.py
@@ -1,1 +1,9 @@
-test
+[
+  {
+    "url": "https://.../komponenter/table/",
+    "title": "Table",
+    "sections": ["Bruk", "Tilgjengelighet"],
+    "text": "Kort utdrag...",
+    "type": "component-doc"
+  }
+]
